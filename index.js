4f4940281f31030893738d1d6c4f93488f0bbdde
David Kadlec <kadlec.david@gmail.com>
2017-06-07 16:53:53 +0200

    Server side fix

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-module.exports = (function(w) {
-	if (!w) {
-		throw 'Window is not available, smartlook-client has to be run in browser environment.'
-	}
-	return {
-		init: function(key) {
-			if (w.smartlook) {
-				throw 'Smartlook client is already initialized.'
-			}
-			w.smartlook = function(){ w.smartlook.api.push(arguments) }
-			w.smartlook.api = []
-			w.smartlook('init', key)
-
-			const head = w.document.getElementsByTagName('head')[0]
-			const script = w.document.createElement('script')
-			script.async = true
-			script.type = 'text/javascript'
-			script.charset = 'utf-8'
-			script.src = 'https://rec.smartlook.com/recorder.js'
-			head.appendChild(script)
-		},
-		tag: function(name, value) {
-			if (!w.smartlook) {
-				throw 'Smartlook client is not initialized.'
-			}
-			w.smartlook('tag', name, value)
-		},
-		disable: function() {
-			if (!w.smartlook) {
-				throw 'Smartlook client is not initialized.'
-			}
-			w.smartlook('disable', true)
+module.exports = ({
+	init: function(key) {
+		if (window.smartlook) {
+			throw 'Smartlook client is already initialized.'
 		}
+		window.smartlook = function(){ window.smartlook.api.push(arguments) }
+		window.smartlook.api = []
+		window.smartlook('init', key)
+
+		const head = window.document.getElementsByTagName('head')[0]
+		const script = window.document.createElement('script')
+		script.async = true
+		script.type = 'text/javascript'
+		script.charset = 'utf-8'
+		script.src = 'https://rec.smartlook.com/recorder.js'
+		head.appendChild(script)
+	},
+	tag: function(name, value) {
+		if (!window.smartlook) {
+			throw 'Smartlook client is not initialized.'
+		}
+		window.smartlook('tag', name, value)
+	},
+	disable: function() {
+		if (!window.smartlook) {
+			throw 'Smartlook client is not initialized.'
+		}
+		window.smartlook('disable', true)
 	}
-})(window)
+})
